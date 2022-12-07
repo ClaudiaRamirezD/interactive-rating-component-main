4208e082bade055f85c7b45213ff18ac410e281d
Claudia Ramirez <clau8812@gmail.com>
2022-12-06 23:14:32 -0600

    All perfect after some tears

--- newJs.js
+++ newJs.js
@@ -7,16 +7,40 @@ let mainCard = document.querySelector(".main-card");
 let thanksCard = document.querySelector(".card-thanks");
 
 
+
+
 // 2. Add event listeners to our items that handle an item click.
+
 
 numberContainer.addEventListener("click", (e) => {
   let numberSelected = e.target.innerText;
   ratingBox.innerText = numberSelected;
 });
 
+
+
 submitBtn.addEventListener("click", () => {
     mainCard.style.display = "none";
     thanksCard.style.display = "flex";
 });
 
+let elements = document.getElementsByClassName("label");
+for (let i = 0; i < elements.length; i++) {
+   
+   elements[i].onclick = function () {
+     // remove class from sibling
 
+     let el = elements[0];
+     console.log(el);
+     while (el) {
+       if (el.tagName === "LABEL") {
+         //remove class
+         el.classList.remove("active");
+       }
+       // pass to the new sibling
+       el = el.nextSibling;
+     }
+
+     this.classList.add("active");
+   };
+ }
